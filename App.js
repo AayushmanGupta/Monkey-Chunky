91c4d7b31552d8d43254415b7f3f9d3ab1777c41
Aayushman Gupta <shailjee073@yahoo.com>
2020-09-02 12:49:42 +0530

    rounded corners of Input Box

--- App.js
+++ App.js
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
-    borderRadius:20,
+    borderRadius:15,
   },
   goButton: {
     width: '50%',
@@ -108,3 +108,9 @@ const styles = StyleSheet.create({
     marginLeft: 95,
   }
 });
+
+
+
+
+
+
